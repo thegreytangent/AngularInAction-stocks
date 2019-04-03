2d3907ae9600da8f406be74e5311a2b227804aa7
jason <thegreytangent@gmail.com>
2019-04-03 15:41:28 +0800

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {}
+
+
+// title = 'stocks';
+//
+// stocks: Array<StockInterface>;
+//
+// constructor( service: StocksService ) {
+//   service.load(['AAPL']).subscribe( stocks => {
+//     this.stocks = stocks
+//   });
+// }
+
+
---
+++ src/app/components/summary/summary.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'summary',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.css']
+})
+export class SummaryComponent implements OnInit {
+  @Input() stock : any;
+
+  constructor() {}
+  ngOnInit() {
+  }
+
+  isNegative() {
+    return ( this.stock && this.stock.change < 0 );
+  }
+
+  isPositive() {
+      return ( this.stock && this.stock.change > 0 );
+  }
+
+}
